d618ec774a31ca60ce77d98bee976038cc9d786e
morbus <morbus@ab05c693-d3d1-0310-8e8b-e1346d90d6a5>
2005-05-20 18:31:44 +0000

    Updated documentation and minor style tweaks.

--- lib/Bot/BasicBot/Pluggable.pm
+++ lib/Bot/BasicBot/Pluggable.pm
@@ -1,20 +1,21 @@
 =head1 NAME
 
-Bot::BasicBot::Pluggable - extension to the simple irc bot base class
-allowing for pluggable modules
+Bot::BasicBot::Pluggable - extended simple IRC bot for pluggable modules
 
 =head1 SYNOPSIS
 
 =head2 Creating the bot module
 
-  # with all defaults
+  # with all defaults.
   my $bot = Bot::BasicBot->new();
 
-  # with useful options
-  my $bot = Bot::BasicBot::Pluggable->new( channels => ["#bottest"],
-
-                      server => "irc.example.com",
-                      port   => "6667",
+  # with useful options. pass any option
+  # that's valid for Bot::BasicBot.
+  my $bot = Bot::BasicBot::Pluggable->new(
+  
+                      channels => ["#bottest"],
+                      server   => "irc.example.com",
+                      port     => "6667",
 
                       nick     => "pluggabot",
                       altnicks => ["pbot", "pluggable"],
@@ -25,8 +26,6 @@ allowing for pluggable modules
 
                 );
 
-  (You can pass any option that's valid for Bot::BasicBot)
-
 =head2 Running the bot (simple)
 
 There's a shell script installed to run the bot.
@@ -34,86 +33,82 @@ There's a shell script installed to run the bot.
   $ bot-basicbot-pluggable.pl --nick MyBot --server irc.perl.org
 
 Then connect to the IRC server, /query the bot, and set a password. See
-L<Bot::BasicBot::Pluggable::Module::Auth> for details.
+L<Bot::BasicBot::Pluggable::Module::Auth> for further details.
 
 =head2 Running the bot (advanced)
 
-There are two useful ways you can use a Pluggable bot. The simple way and the
-flexible way. The simple way is:
+There are two useful ways to create a Pluggable bot. The simple way is:
 
-  # Load some useful modules
+  # Load some useful modules.
   my $infobot_module = $bot->load("Infobot");
   my $google_module = $bot->load("Google");
   my $seen_module = $bot->load("Seen");
 
-  # Set the google key (see http://www.google.com/apis/)
+  # Set the Google key (see http://www.google.com/apis/).
   $google_module->set("google_key", "some google key");
-  
+
   $bot->run();
 
-This lets you run a bot with a few modules, but not change those modules
-during the run of the bot. The complex way is as follows:
+The above lets you run a bot with a few modules, but not change those modules
+during the run of the bot. The complex, but more flexible, way is as follows:
 
-  # Load the loader module
+  # Load the Loader module.
   $bot->load('Loader');
-  
-  # run the bot
+
+  # run the bot.
   $bot->run();
 
-This is simpler but needs setup once the bot is joined to a server. the Loader
-module lets you talk to the bot in-channel and tell it to load and unload other
-modules. The first one you'll want to load is the 'Auth' module, so that other
-people can't load and unload modules without permission. Then you need to log in
-as an admin and change your password.
+This is simpler but needs further setup once the bot is joined to a server. The
+Loader module lets you talk to the bot in-channel and tell it to load and unload
+other modules. The first one you'll want to load is the 'Auth' module, so that
+other people can't load and unload modules without permission. Then you'll need
+to log in as an admin and change the default password, per the following /query:
 
-  (in a query)
   !load Auth
   !auth admin julia
   !password julia new_password
   !auth admin new_password
-  
-Once you've done this, your bot is safe against other IRC users. Now you can tell
-it to load and unload other modules any time:
+
+Once you've done this, your bot is safe from other IRC users, and you can tell
+it to load and unload other installed modules at any time. Further information
+on module loading is in L<Bot::BasicBot::Pluggable::Module::Loader>.
 
   !load Seen
   !load Google
   !load Join
 
-The join module lets you tell the bot to join and leave channels:
+The Join module lets you tell the bot to join and leave channels:
 
   !join #mychannel
   !leave #someotherchannel
-  
-The perldoc pages for the various modules will list other commands.
 
+The perldoc pages for the various modules will list other commands.
 
 =head1 DESCRIPTION
 
 Bot::BasicBot::Pluggable started as Yet Another Infobot replacement, but now
-is a generalised framework for writing infobot-type bots, that lets you keep
-each function seperate. You can have seperate modules for factoid tracking,
-'seen' status, karma, googling, etc. Included with the package are modules
-for:
+is a generalised framework for writing infobot-type bots that lets you keep
+each specific function seperate. You can have seperate modules for factoid
+tracking, 'seen' status, karma, googling, etc. Included default modules are
+below. Use C<perldoc Bot::BasicBot::Pluggable::Module::<module name>> for help
+on their individual terminology.
 
-  Auth - user authentication and admin access
-  Loader - loads and unloads modules as bot commands
-  Join - joins and leaves channels
-  Vars - changes module variables
-  Infobot - handles infobot-style factoids
-  Karma - tracks the popularity of things
-  Seen - tells you when people were last seen
-  DNS - host lookup
-  Google - search google for things
-  Title - Gets the title of pages mentioned in channel
+  Auth    - user authentication and admin access.
+  DNS     - host lookup (e.g. nslookup and dns).
+  Google  - search Google for things.
+  Infobot - handles infobot-style factoids.
+  Join    - joins and leaves channels.
+  Karma   - tracks the popularity of things.
+  Loader  - loads and unloads modules as bot commands.
+  Seen    - tells you when people were last seen.
+  Title   - gets the title of URLs mentioned in channel.
+  Vars    - changes module variables.
 
-use perldoc Bot::BasicBot::Pluggable::Module::<module name> for help on
-their terminology.
-
-The way this works is very simple. You create a new bot object, and tell it
-to load various modules. Then you run the bot. The modules get events when
-the bot sees things happen, and can respond to the events.
-
-perldoc Bot::BasicBot::Pluggable::Module::Base for the details of the module API.
+The way the Pluggable bot works is very simple. You create a new bot object
+and tell it to load various modules (or, alternatively, load just the Loader
+module and then interactively load modules via an IRC /query). The modules
+receive events when the bot sees things happen and can, in turn, respond. See
+C<perldoc Bot::BasicBot::Pluggable::Module> for the details of the module API.
 
 =cut
 
@@ -136,8 +131,8 @@ sub init {
 
   # the default store is a SQLite store
   $self->{store} ||= {
-    type => "DBI",
-    dsn => "dbi:SQLite:bot-basicbot.sqlite",
+    type  => "DBI",
+    dsn   => "dbi:SQLite:bot-basicbot.sqlite",
     table => "basicbot",
   };
 
@@ -157,19 +152,18 @@ sub init {
   return 1;
 }
 
-=head2 Main Methods
+=head2 Methods
 
 =over 4
 
-=item new
+=item new( key => value, ... )
 
-Create a new Bot. Identical to the new method in Bot::BasicBot.
+Create a new Bot. Identical to the C<new> method in L<Bot::BasicBot>.
 
 =item load($module)
 
-Load a module for the bot by name, from ./modules/Modulename.pm if that file
-exists, and falling back to the system package
-Bot::BasicBot::Pluggable::Module::$module if not.
+Load a module for the bot by name from C<./modules/ModuleName.pm> if that file
+exists, and falling back to C<Bot::BasicBot::Pluggable::Module::$module> if not.
 
 =cut
 
@@ -177,17 +171,17 @@ sub load {
   my $self = shift;
   my $module = shift;
 
-  # it's safe to die here, mostly this call is evaled
+  # it's safe to die here, mostly this call is eval'd.
   die "Need name" unless $module;
   die "Already loaded" if $self->handler($module);
 
   # This is possible a leeeetle bit evil.
-  print STDERR "Loading module '$module'.. ";
+  print STDERR "Loading module '$module' ";
   my $file = "Bot/BasicBot/Pluggable/Module/$module.pm";
   $file = "./modules/$module.pm" if (-e "./modules/$module.pm");
-  print STDERR "from file $file\n";
-  
-  # force a reload of the file (in the event that we've already loaded it)
+  print STDERR "from file $file.\n";
+
+  # force a reload of the file (in the event that we've already loaded it).
   no warnings 'redefine';
   delete $INC{$file};
   require $file;
@@ -205,12 +199,10 @@ sub load {
 
 =item reload($module)
 
-Reload the module $module - equivalent to unloading it (if it's already
+Reload the module C<$module> - equivalent to unloading it (if it's already
 loaded) and reloading it. Will stomp the old module's namespace - warnings
-are expected here.
-
-Not toally clean - if you're experiencing odd bugs, restart the bot if
-possible. Works for minor bug fixes, etc.
+are expected here. Not toally clean - if you're experiencing odd bugs, restart
+the bot if possible. Works for minor bug fixes, etc.
 
 =cut
 
@@ -222,7 +214,7 @@ sub reload {
   return $self->load($module);
 }
 
-=item unload
+=item unload($module)
 
 Removes a module from the bot. It won't get events any more.
 
@@ -233,27 +225,25 @@ sub unload {
   my $module = shift;
   return "Need name" unless $module;
   return "Not loaded" unless $self->handler($module);
-  warn "Unloading module '$module'..\n";
-
+  warn "Unloading module '$module' ";
   $self->remove_handler($module);
-  return "Removed";
 }
 
 =item module($module)
 
-returns the handler object for the loaded module '$module'. used, eg, to get
-the 'Auth' hander to check if a given user is authenticated.
+Returns the handler object for the loaded module C<$module>. Used, e.g.,
+to get the 'Auth' hander to check if a given user is authenticated.
 
 =cut
 
 sub module {
   my $self = shift;
   return $self->handler(@_);
-}    
+}
 
 =item modules
 
-returns a list of the names of all loaded modules, as an array.
+Returns a list of the names of all loaded modules as an array.
 
 =cut
 
@@ -275,11 +265,11 @@ sub handlers {
   return \@keys;
 }
 
-=head2 add_handler(handler object, name of handler)
+=item add_handler($handler_object, $handler_name)
 
-adds a handler object with the given name to the queue of modules. There
-is no order specified internally, adding a module earlier does not
-guarantee it gets called first. Names must be unique.
+Adds a handler object with the given name to the queue of modules. There
+is no order specified internally, so adding a module earlier does not
+guarantee it'll get called first. Names must be unique.
 
 =cut
 
@@ -287,12 +277,12 @@ sub add_handler {
   my ($self, $handler, $name) = @_;
   die "Need a name for adding a handler" unless $name;
   die "Can't load a handler with a duplicate name $name" if $self->{handlers}{lc($name)};
-  $self->{handlers}{lc($name)} = $handler;    
+  $self->{handlers}{lc($name)} = $handler;
 }
 
-=head2 remove_handler
+=item remove_handler($handler_name)
 
-remove a handler with the given name.
+Remove a handler with the given name.
 
 =cut
 
@@ -301,12 +291,11 @@ sub remove_handler {
   die "Need a name for removing a handler" unless $name;
   die "Hander $name not defined" unless $self->{handlers}{lc($name)};
   delete $self->{handlers}{lc($name)};
-  return "Done.";
 }
 
-=head2 store
+=item store
 
-returns the object store associated with the bot. See L<Bot::BasicBot::Pluggable::Store>.
+Returns the bot's object store; see L<Bot::BasicBot::Pluggable::Store>.
 
 =cut
 
@@ -319,13 +308,9 @@ sub store {
   return $self->{store_object};
 }
 
-####################################################
-# ..from Bot::BasicBot:
+=item dispatch($method_name, $method_params)
 
-=head2 dispatch(method name, params)
-
-call the named method on every loaded module, if the module has a method
-with that name.
+Call the named C<$method> on every loaded module with that method name.
 
 =cut
 
@@ -341,17 +326,48 @@ sub dispatch {
   return undef;
 }
 
+=item help
+
+Returns help for the ModuleName of message 'help ModuleName'. If no message
+has been passed, return a list of all possible handlers to return help for.
+
+=cut
+
+sub help {
+  my $self = shift;
+  my $mess = shift;
+  $mess->{body} =~ s/^help\s*//i;
+  
+  unless ($mess->{body}) {
+    return "Ask me for help about: " . join(", ", $self->handlers())." (say 'help <modulename>').";
+  } else {
+    if (my $handler = $self->handler($mess->{body})) {
+      my $help;
+      eval "\$help = \$handler->help(\$mess);";
+      return "Error calling help for handler $mess->{body}: $@" if $@;
+      return $help;
+    } else {
+      return "I don't know anything about '$mess->{body}'.";
+    }
+  }
+}
+
+=item run
+
+Runs the bot. POE core gets control at this point; you're unlikely to get it back.
+
+=back
+
+=cut
+
+#########################################################
+# the following routines are lifted from Bot::BasicBot: #
+#########################################################
 sub tick {
   my $self = shift;
   $self->dispatch('tick');
   return 5;
 }
-
-=head2 said
-
-called as a subclass of Bot::BasicBot, 
-
-=cut
 
 sub said {
   my $self = shift;
@@ -395,25 +411,6 @@ sub emoted {
   return undef;
 }
 
-sub help {
-  my $self = shift;
-  my $mess = shift;
-  $mess->{body} =~ s/^help\s*//i;
-  
-  unless ($mess->{body}) {
-    return "Ask me for help about: " . join(", ", $self->handlers())." (say 'help <modulename>')";
-  } else {
-    if (my $handler = $self->handler($mess->{body})) {
-      my $help;
-      eval "\$help = \$handler->help(\$mess); ";
-      return "Error calling help for handler $mess->{body}: $@" if $@;
-      return $help;
-    } else {
-      return "I don't know anything about '$mess->{body}'.";
-    }
-  }
-}
-
 sub connected {
   my $self = shift;
   warn "Bot::BasicBot::Pluggable connected\n";
@@ -428,13 +425,6 @@ sub chanpart {
   shift->dispatch("chanpart", @_);
 }
 
-=item run
-
-runs the bot. The POE core gets control as of this point, you're unlikely to
-get control back.
-
-=back
-
 =head1 AUTHOR
 
 Tom Insam E<lt>tom@jerakeen.orgE<gt>
@@ -445,58 +435,46 @@ and/or modify it under the same terms as Perl itself.
 =head1 CREDITS
 
 Bot::BasicBot was written initially by Mark Fowler, and worked on heavily by
-Simon Kent, who was kind enough to apply some patches I needed for
-Pluggable.
-
-Eventually.
-
-Oh, yeah, and I stole huge chunks of docs from the Bot::BasicBot source,
-too.
+Simon Kent, who was kind enough to apply some patches I needed for Pluggable.
+Eventually. Oh, yeah, and I stole huge chunks of docs from the Bot::BasicBot
+source too. I spent a lot of time in the mozbot code, and that has influenced
+my ideas for Pluggable. Mostly to get round its awfulness.
 
 Various people helped with modules. Convert was almost ported from the
-infobot code by blech. But not quite. Thanks for trying.. blech has also put
-a lot of effort into the chump.cgi/chump.tem in the examples/ folder,
+infobot code by blech. But not quite. Thanks for trying... blech has also put
+a lot of effort into the chump.cgi & chump.tem files in the examples/ folder,
 including some /inspired/ calendar evilness.
 
-And thanks to the rest of #2lmc, who were my unwilling guinea pigs during
+And thanks to the rest of #2lmc who were my unwilling guinea pigs during
 development. And who kept suggesting totally stupid ideas for modules that I
-then felt compelled to go implement. Shout.pm owes it's existence to #2lmc.
-
-I spent a lot of time in the mozbot code, and that has influenced my ideas
-for Pluggable. Mostly to get round its awfulness.
+then felt compelled to go implement. Shout.pm owes its existence to #2lmc.
 
 =head1 SYSTEM REQUIREMENTS
 
-Bot::BasicBot::Pluggable is based on POE, and really needs the latest
-version. Because POE is like that sometimes.
-
-You also need POE::Component::IRC. Oh, and Bot::BasicBot.
-
-Some of the modules will need more modules. eg, Google.pm needs Net::Google.
-See the module docs for more details.
+Bot::BasicBot::Pluggable is based on POE, and really needs the latest version.
+Because POE is like that sometimes. You also need L<POE::Component::IRC>.
+Oh, and L<Bot::BasicBot>. Some of the modules will need more modules, e.g.
+Google.pm needs L<Net::Google>. See the module docs for more details.
 
 =head1 BUGS
 
-During the make, make test make install process, POE will moan about
-its kernel not being run. This is a Bot::BasicBot problem, apparently.
-
-reloading a module causes warnings as the old module gets it's namespace
-stomped. Not a lot you can do about that.
-
-All modules need to be in the Bot::Pluggable::Module:: namespace. Well,
-that's not really a bug.                                                                                       
-
-More other things than I can shake a stick at.
+During the C<make>, C<make test>, C<make install> process, POE will moan about
+its kernel not being run. This is a C<Bot::BasicBot problem>, apparently.
+Reloading a module causes warnings as the old module gets its namespace stomped.
+Not a lot you can do about that. All modules must be in Bot::Pluggable::Module::
+namespace. Well, that's not really a bug.                                                                                       
 
 =head1 SEE ALSO
 
 POE
 
-POE::Component::IRC
+L<POE::Component::IRC>
 
-Bot::BasicBot
+L<Bot::BasicBot>
 
-Possibly Infobot, at http://www.infobot.org, and Mozbot, somewhere in mozilla.org.
+Infobot: http://www.infobot.org/
+
+Mozbot: http://www.mozilla.org/projects/mozbot/
 
 =cut
 
