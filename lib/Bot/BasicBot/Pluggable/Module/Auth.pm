061822ce22681273399bdaa6a1b402b8975b19e5
morbus <morbus@ab05c693-d3d1-0310-8e8b-e1346d90d6a5>
2005-05-21 02:49:21 +0000

    Auth: Revised documentation, switched to admin() not said().

--- lib/Bot/BasicBot/Pluggable/Module/Auth.pm
+++ lib/Bot/BasicBot/Pluggable/Module/Auth.pm
@@ -1,73 +1,72 @@
 =head1 NAME
 
-Bot::BasicBot::Pluggable::Module::Auth
+Bot::BasicBot::Pluggable::Module::Auth - authentication for Bot::BasicBot::Pluggable modules
 
 =head1 SYNOPSIS
 
-Authentication for Bot::BasicBot::Pluggable modules
-
-This module catches messages at priority '1' and stops anything starting
-with '!' unless the user is authed, so most admin modules, eg Loader, can
-merely sit at priority 2, and assume that the user is authed if the command
-reaches them.
-
-If you want to use modules that can change bot state, like Loader or Vars,
-you almost certainly want this module.
+This module catches messages at priority 1 and stops anything starting
+with '!' unless the user is authed. Most admin modules, e.g. Loader, can
+merely sit at priority 2 and assume the user is authed if the !command
+reaches them. If you want to use modules that can change bot state, like
+Loader or Vars, you almost certainly want this module.
 
 =head1 IRC INTERFACE
 
-Commands:
+The default user is 'admin' with password 'julia'. Change this.
 
 =over 4
 
 =item !auth <username> <password>
 
-Authenticate to the bot with a username and password. Logins timeout after
-an hour, so you'll have to re-auth if you've been away.
+Authenticate as an administrators. Logins timeout after an hour.
 
 =item !adduser <username> <password>
 
-Adds a user with the given password
+Adds a user with the given password.
 
-=item !password <oldpassword> <new password>
+=item !deluser <username>
 
-Change your current password (must be logged in)
+Deletes a user. Don't delete yourself, that's probably not a good idea.
+
+=item !password <old password> <new password>
+
+Change your current password (must be logged in first).
 
 =item !users
 
 List all the users the bot knows about.
 
-=item deluser <username>
+=back
 
-Deletes a user. Don't delete yourself, that's probably not a good idea.
+=head1 METHODS
+
+The only useful method is C<authed()>:
+
+=over 4
+
+=item authed($username)
+
+Returns 1 if the given username is logged in, 0 otherwise:
+
+  if ($bot->module("Auth")->authed("jerakeen")) { ... }
 
 =back
 
-The default user is 'admin', password 'julia'. Change this.
+=head1 AUTHOR
 
+Tom Insam E<lt>tom@jerakeen.orgE<gt>
 
-=head1 MODULE INTERFACE
-
-The only useful command is 'authed':
-
-  if ($bot->module("Auth")->authed("jerakeen")) {
-  ..
-
-returns 1 if the given nick is logged in, 0 otherwise.
-
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
 
 =head1 BUGS
 
-All users are admins. This is fine at the moment, as the only things that
-need you to be logged in are admin functions.
-
-Passwords are stored in plaintext, and are trivial to extract for any module
-on the system. I don't consider this a bug, because I assume you trust the
-modules you're loading.
-
-If Auth is /not/ loaded, all users effectively have admin permissions.
-This may not be a good idea, but is also not an Auth bug, it's an
-architecture bug.
+All users are admins. This is fine at the moment, as the only things that need
+you to be logged in are admin functions. Passwords are stored in plaintext, and
+are trivial to extract for any module on the system. I don't consider this a
+bug, because I assume you trust the modules you're loading. If Auth is I<not>
+loaded, all users effectively have admin permissions. This may not be a good
+idea, but is also not an Auth bug, it's an architecture bug.
 
 =cut
 
@@ -78,7 +77,6 @@ use strict;
 
 sub init {
     my $self = shift;
-    
     unless ( $self->get("password_admin") ) {
         $self->set("password_admin" => "julia"); # mmmm, defaults.
     }
@@ -86,29 +84,27 @@ sub init {
 
 sub help {
     my ($self, $bot, $mess) = @_;
-    return "Authenticator for admin-level commands. usage: !auth <pass> to authenticate";
+    return "Authenticator for admin-level commands. Usage: !auth <username> <password>, !adduser <username> <password>, !deluser <username>, !password <old password> <new password>, !users.";
 }
 
-sub said {
-    my ($self, $mess, $pri) = @_;
+sub admin {
+    my ($self, $mess) = @_;
     my $body = $mess->{body};
 
-    return unless ($pri == 1);
     return unless ($body and length($body) > 4);
 
-    # we don't care about commands that don't start with '!'
+    # we don't care about commands that don't start with '!'.
     return 0 unless $body =~ /^!/;
 
-    # system commands have to be directly addressed.
+    # system commands have to be directly addressed...
     return 1 unless $mess->{address};
 
-    # ..and in privmsg
-    return "Admin commands in privmsg only, please"
+    # ...and in a privmsg.
+    return "Admin commands in privmsg only, please."
       unless !defined $mess->{channel} || $mess->{channel} eq 'msg';
 
     if ($body =~ /^!auth\s+(\w+)\s+(\w+)/) {
-        my $user = $1;
-        my $pass = $2;
+        my ($user, $pass) = ($1, $2);
         my $stored = $self->get("password_".$user);
 
         if ( $pass and $stored and $pass eq $stored ) {
@@ -121,77 +117,67 @@ sub said {
 
         } else {
             delete $self->{auth}{$mess->{who}};
-            return "Bad password";
+            return "Wrong password.";
         }
 
     } elsif ($body =~ /^!auth/) {
-        return "Bad auth. usage: !auth <username> <password>\n";
-        
+        return "Usage: !auth <username> <password>.";
+
     } elsif ($body =~ /^!adduser\s+(\w+)\s+(\w+)/) {
-        my $user = $1;
-        my $pass = $2;
+        my ($user, $pass) = ($1, $2);
         if ($self->authed($mess->{who})) {
             $self->set( "password_".$user, $pass );
-            return "Added user $user";
+            return "Added user $user.";
         } else {
             return "You need to authenticate.";
         }
 
     } elsif ($body =~ /^!adduser/) {
-        return "usage: adduser <username> <password>";
+        return "Usage: !adduser <username> <password>";
 
     } elsif ($body =~ /^!deluser\s+(\w+)/) {
         my $user = $1;
         if ($self->authed($mess->{who})) {
             $self->unset( "password_".$user );
-            return "Deleted user $user";
+            return "Deleted user $user.";
         } else {
             return "You need to authenticate.";
         }
 
     } elsif ($body =~ /^!deluser/) {
-        return "usage: deluser <username>";
+        return "Usage: !deluser <username>";
 
     } elsif ($body =~ /^!passw?o?r?d?\s+(\w+)\s+(\w+)/) {
-        my $old_pass = $1;
-        my $pass = $2;
+        my ($old_pass, $pass) = ($1, $2);
         if ($self->authed($mess->{who})) {
             my $username = $self->{auth}{$mess->{who}}{username};
             if ( $old_pass eq $self->get("password_$username") ) {
                 $self->set("password_$username", $pass);
-                return "Changed password to $pass";
+                return "Changed password to $pass.";
             } else {
-                return "wrong password";
+                return "Wrong password.";
             }
 
         } else {
             return "You need to authenticate.";
         }
+
     } elsif ($body =~ /^!passw?o?r?d?/) {
-        return "usage: !passwd <old password> <newpassword>";
+        return "Usage: !password <old password> <new password>.";
 
     } elsif ($body =~ /^!users/) {
-        return "Users: ".join(", ", map { s/^password_// ? $_ : () } $self->store_keys);
+        return "Users: ".join(", ", map { s/^password_// ? $_ : () } $self->store_keys).".";
 
     } else {
-
-        if ($self->authed($mess->{who})) {
-            return undef;
-        } else {
-            return "You need to authenticate.";
-        }
+        return $self->authed($mess->{who}) ? undef : "You need to authenticate.";
     }
-    return "Bad fallthrough.";
 }
 
 sub authed {
     my ($self, $username) = @_;
-
     return 1 if ($self->{auth}{$username}{time}
              and $self->{auth}{$username}{time} + 7200 > time());
-
     return 0;
-    
 }
 
 1;
