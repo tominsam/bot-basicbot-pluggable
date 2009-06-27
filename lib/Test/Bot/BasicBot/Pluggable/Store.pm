d1f47676916820f13e89379fb3603a81aaf15216
Mario Domgoergen <dom@math.uni-bonn.de>
2009-06-27 18:46:03 +0200

    documented module

--- lib/Test/Bot/BasicBot/Pluggable/Store.pm
+++ lib/Test/Bot/BasicBot/Pluggable/Store.pm
@@ -21,3 +21,39 @@ sub store_ok {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Test::Bot::BasicBot::Pluggable::Store - basics tests for Bot::BasicBot::Pluggable storage classes
+
+=head1 SYNOPSIS
+
+  store_ok( 'Memory' );
+  store_ok( 'Deep', { file => 'deep.db' });
+
+=head1 DESCRIPTION
+
+This modules collects some general functions to test storage module
+sfor Bot::BasicBot::Pluggable. In the moment we just export the
+basic store_ok.
+
+=head1 FUNCTIONS
+
+=head2 store_ok
+
+This functions justs tests some basic behaviour every storage module
+should provide, like store creation, get and set. You can't use it
+directly with Test::More as we harcode the number of tests to nine
+in the moment. (Man, i'm so excited about nested tap streams in the
+newest development release of Test::Simple)
+
+=head1 AUTHOR
+
+Mario Domgoergen <mario@domgoergen.com>
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+=cut
